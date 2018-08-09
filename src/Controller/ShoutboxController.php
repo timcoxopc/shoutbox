997ba35b21c01dc2b07b091a4247c0519e21c3df
Tim Cox <tim.cox@opc.com.au>
2018-08-09 13:05:53 +1000

    Mod

--- src/Controller/ShoutboxController.php
+++ src/Controller/ShoutboxController.php
@@ -1,11 +1,49 @@
 <?php 
 
 namespace Drupal\shoutbox\Controller;
-
+use Drupal\Component\Render\FormattableMarkup;
 use Symfony\Component\HttpFoundation\Response;
 
 class ShoutboxController {
-	public function create_page() {
-		return new Response('Test');
-	}
+  public function createAdminPage() {
+    $params = \Drupal::request()->get('keys');
+    //kint($params);
+
+    if(isset($params['delete'])) {
+      drupal_set_message('Message has been deleted');
+      $num_deleted = db_delete('shoutbox')
+      ->condition('id', $params['delete'])
+      ->execute();
+    }
+
+    $result = db_select('shoutbox', 's')
+      ->fields('s', ['id', 'timestamp', 'name', 'message'])
+      ->orderBy('timestamp', 'DESC')
+      ->execute()
+      ->fetchAll();
+
+    $headers = [
+      'timestamp' => 'Timestamp',
+      'name' => 'Name',
+      'message' => 'Message',
+      'admin' => 'Admin'
+    ];
+
+    foreach ($result as $row) {
+      $rows[] = [
+        'timestamp' => date('d/m/Y', $row->timestamp),
+        'name' => t('<strong>' . $row->name . '</strong>'),
+        'message' => t('<p>' . $row->message . '</p>'),
+        'delete' => t('<button formmethod="get" formaction="" type="submit" name="delete" value="' . $row->id . '">Delete</button>'),
+      ];
+    }
+
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+    ];
+
+    return $form;
+  }
 }
